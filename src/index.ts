ab0644bcd2895fa50892c751c958bd848e58c9f4
ME.AT <meatish2008@gmail.com>
2025-02-07 20:11:23 +0330

    refactor: refactor mongoDB connection and add new folder

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,17 @@
 import * as express from "express";
-import mongoose from "mongoose";
 import * as dotenv from "dotenv";
+import * as mongoose from "./config/db";
 
 dotenv.config();
-
+mongoose;
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/todo";
 
 app.use(express.json());
-
-mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  } as mongoose.ConnectOptions)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.get("/", (req, res) => {
   res.send("Welcome to the To-Do List API!");
 });
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
