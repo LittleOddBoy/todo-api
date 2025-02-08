d0e7c648d5fc92e0103ca31d7ce4e334fdb18c47
Amirhossein Kalari <hosein.kalari.1387@gmail.com>
2025-02-08 15:09:58 +0330

    fix: remove deprected features of mongoose

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,9 +2,6 @@ import mongoose from "mongoose";
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/todo";
 
 mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  } as mongoose.ConnectOptions)
+  .connect(MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
